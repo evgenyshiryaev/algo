13bc9dd7d64a68ad167a998d0624e7cc4c8f74c6
Evgeny Shiryaev <evgeny.shiryaev@devpool.com>
2022-09-04 13:15:52 +0300

    Add find interval max to monostack

--- src/main/java/algo/ds/MonotonicStack.java
+++ src/main/java/algo/ds/MonotonicStack.java
@@ -2,26 +2,62 @@ package algo.ds;
 
 import java.util.Arrays;
 
+
 public class MonotonicStack {
 
-    public static int[] getMax(int[] nums) {
+    // O(n)
+    public static int[] getRightGreaters(int[] nums) {
         int N = nums.length;
 
         int[] s = new int[N];
         int si = 0;
 
-        int[] max = new int[N];
-        Arrays.fill(max, -1);
+        int[] greaters = new int[N];
+        Arrays.fill(greaters, -1);
 
         for (int i = 0; i < N; ++ i) {
             int num = nums[i];
             while (si > 0 && num > nums[s[si - 1]]) {
-                max[s[-- si]] = i;
+                greaters[s[-- si]] = i;
             }
             s[si ++] = i;
         }
 
-        return max;
+        return greaters;
+    }
+
+
+    // O(n)
+    public static int[] getIntervalMaxs(int[] nums, int m) {
+        int N = nums.length;
+
+        int[] s = new int[N];
+        int si = 0;
+        int sj = 0;
+
+        int[] maxs = new int[N - m + 1];
+
+        for (int i = 0; i < m; ++ i) {
+            while (si < sj && nums[i] >= nums[s[sj - 1]]) {
+                -- sj;
+            }
+            s[sj ++] = i;
+        }
+        maxs[0] = nums[s[si]];
+
+        for (int i = m; i < N; ++ i) {
+            while (si < sj && s[si] <= i - m) {
+                ++ si;
+            }
+            while (si < sj && nums[i] >= nums[s[sj - 1]]) {
+                -- sj;
+            }
+            s[sj ++] = i;
+
+            maxs[i - m + 1] = nums[s[si]];
+        }
+
+        return maxs;
     }
 
 }
--- src/test/java/algo/ds/MonotonicStackTest.java
+++ src/test/java/algo/ds/MonotonicStackTest.java
@@ -6,10 +6,39 @@ import org.junit.jupiter.api.Test;
 public class MonotonicStackTest {
 
     @Test
-    public void test() {
+    public void getRightGreatersTest() {
         Assertions.assertArrayEquals(
                 new int[] {-1, 2, 7, 4, 7, 6, 7, -1},
-                MonotonicStack.getMax(new int[] {10, 1, 6, 2, 4, 1, 4, 9}));
+                MonotonicStack.getRightGreaters(new int[] {10, 1, 6, 2, 4, 1, 4, 9}));
+    }
+
+
+    @Test
+    public void getIntervalMaxsTest() {
+        Assertions.assertArrayEquals(
+                new int[] {10, 1, 6, 2, 4, 1, 4, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 1));
+        Assertions.assertArrayEquals(
+                new int[] {10, 6, 6, 4, 4, 4, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 2));
+        Assertions.assertArrayEquals(
+                new int[] {10, 6, 6, 4, 4, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 3));
+        Assertions.assertArrayEquals(
+                new int[] {10, 6, 6, 4, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 4));
+        Assertions.assertArrayEquals(
+                new int[] {10, 6, 6, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 5));
+        Assertions.assertArrayEquals(
+                new int[] {10, 6, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 6));
+        Assertions.assertArrayEquals(
+                new int[] {10, 9},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 7));
+        Assertions.assertArrayEquals(
+                new int[] {10},
+                MonotonicStack.getIntervalMaxs(new int[] {10, 1, 6, 2, 4, 1, 4, 9}, 8));
     }
 
 }
