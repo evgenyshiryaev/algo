76d410aab27a82f9c04652cf20df75819b5edda0
Evgeny Shiryaev <arpmipg@gmail.com>
2024-06-11 22:44:30 +0300

    Update Java algo template

--- java/src/main/java/Solution.java
+++ java/src/main/java/Solution.java
@@ -4,20 +4,39 @@ import java.io.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-
-        // test case number
-        int tcs = in.nextInt();
+        int tcs = FastScanner.nextInt();
 
         for (int tc = 1; tc <= tcs; ++tc) {
-            int n = in.nextInt();
+            int n = FastScanner.nextInt();
 
             int[] a = new int[n];
             for (int i = 0; i < n; ++i) {
-                a[i] = in.nextInt();
+                a[i] = FastScanner.nextInt();
             }
 
             System.out.println("RESULT");
+        }
+    }
+
+
+    private static class FastScanner {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st = new StringTokenizer("");
+
+        static String nextString() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        static int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        static long nextLong() {
+            return Long.parseLong(nextString());
         }
     }
 
